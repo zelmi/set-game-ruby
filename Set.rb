80f252f67e1c9d10cec6e99331d487d5161a5c0f
EstherH1 <47611533+EstherH1@users.noreply.github.com>
2021-01-31 00:39:50 -0500

    Update Set.rb

--- Set.rb
+++ Set.rb
@@ -104,14 +104,14 @@ ResetDeck() do
 		col = colors.sample
 		shad = shading.sample
 		num = numbers.sample
-		current_cards.push(Card.new(nil, sym, col, shad, num))
-		unique_deck = current_cards.uniq.each{|card|, card.symbol, card.color, card.shading, card.number}
-		if unique_deck.length != current_cards.length
-			current_cards = unique_deck
+		$current_cards.push(Card.new(nil, sym, col, shad, num))
+		unique_deck = $current_cards.uniq.each{|card|, card.symbol, card.color, card.shading, card.number}
+		if unique_deck.length != $current_cards.length
+			$current_cards = unique_deck.clone
 		end
 	end
 		for i in 1..12 do
-			current_cards[i].card_id = i
+			$current_cards[i].card_id = i
 		end
 		
 end
