1853adae78b59f536af7467469893b48fb9d5689
Nick Springer <53102753+springer268@users.noreply.github.com>
2021-02-01 17:41:35 -0500

    Add files via upload

--- Set2.rb
+++ Set2.rb
@@ -1,81 +1,82 @@
 def prompt(message)
     puts message
-    gets.chomp()
+    gets.chomp!()
 end
 
 class Card
-	initialize(color, symbol, shading, amount)
+	attr_accessor :color, :symbol, :shading, :amount
+
+	def initialize(color, symbol, shading, amount)
 		@color = color
 		@symbol = symbol
 		@shading = shading
 		@amount = amount
     end
 
-	equals(other)
-		return (
-			@amount == other.amount &&
-			@color == other.color &&
-			@shading == other.shading &&
-			@symbol == other.symbol
-		)
+	def equals(other)
+		@amount == other.amount && @color == other.color && @shading == other.shading && @symbol == other.symbol
 	end
 
-	toString()
-		return "#{this.color} #{this.symbol} #{this.shading} #{this.amount}"
+	def toString
+		"#{@color} #{@symbol} #{@shading} #{@amount}"
     end
 end
 
 class Player
-	initialize(name)
+	attr_accessor :name, :score, :winCount
+
+	def initialize(name)
 		@name = name
 		@score = 0
 		@winCount = 0
     end
 end
 
-cardsAvailable = []
-cardsDisplayed = []
-cardsUsed = []
-players = []
+$cardsAvailable = []
+$cardsDisplayed = []
+$cardsUsed = []
+$players = []
 
 def generateDeck()
 	for color in 0...3
 		for symbol in 0...3
 			for shading in 0...3
 				for amount in 0...3
-					cardsAvailable.push(Card.new(color, symbol, shading, amount))
+					$cardsAvailable.push(Card.new(color, symbol, shading, amount))
                 end
 			end
 		end
 	end
 end
 
-def addRandomCardFromAvailableToDisplayed()
-	if cardsAvailable.length == 0 return
+def addRandomCardFromAvailableToDisplayed
+	if $cardsAvailable.length == 0 
+		return
+	end
 
-	randomCardIndex = rand(0...cardsAvailable.length)
-	randomCard = cardsAvailable.delete_at(randomCardIndex)
+	randomCardIndex = rand(0...$cardsAvailable.length)
+	randomCard = $cardsAvailable.delete_at(randomCardIndex)
 
-    cardsDisplayed.push(randomCard)
+    $cardsDisplayed.push(randomCard)
 end
 
 def addRandomAvailableCardsToDisplayed(numPlayers)
 	for i in 0...numPlayers
 		addRandomCardFromAvailableToDisplayed()
-    end
+	end
 end
 
 def addThreeDisplayedCardsToUsed(cardOne, cardTwo, cardThree)
-	cardsDisplayed = cardsDisplayed.filter {|card| !(card.equals(cardOne) || card.equals(cardTwo) || card.equals(cardThree))}
-	cardsUsed.push(cardOne, cardTwo, cardThree)
+	$cardsDisplayed = $cardsDisplayed.filter {|card| !(card.equals(cardOne) || card.equals(cardTwo) || card.equals(cardThree))}
+	$cardsUsed.push(cardOne, cardTwo, cardThree)
 end
 
 def shuffleDeck()
-	cardsAvailable.push(*cardsUsed)
-	cardsAvailable.push(*cardsDisplayed)
+	$cardsAvailable.push(*$cardsUsed)
+	$cardsAvailable.push(*$cardsDisplayed)
 
-	cardsDisplayed = []
-	cardsUsed = []
+	$cardsDisplayed = []
+	$cardsUsed = []
 end
 
 def isSet(cardOne, cardTwo, cardThree)
@@ -94,38 +95,47 @@ def isSet(cardOne, cardTwo, cardThree)
 	allDifferentOrSameShading = allSameShading || allDifferentShading
 	allDifferentOrSameSymbol = allSameSymbol || allDifferentSymbol
 
-	return allDifferentOrSameNumber && allDifferentOrSameColor && allDifferentOrSameShading && allDifferentOrSameSymbol
+	allDifferentOrSameNumber && allDifferentOrSameColor && allDifferentOrSameShading && allDifferentOrSameSymbol
 end
 
-def playSet()
-	while cardsDisplayed.length >= 3 do
-		input = prompt("Which are sets?#{cardsDisplayed.map({|card, i| "\n#{i}: #{card.toString()}"}).join("")}")
+def playSet
+	while $cardsDisplayed.length >= 3 do
+		inputString = "Which are sets? (#{$cardsAvailable.length} left) #{$cardsDisplayed.enum_for(:each_with_index).map{|card, i| "\n#{i}: #{card.toString()}"}.join("")}"
 
-		if input != nil
+		input = prompt(inputString)
+
+		if input != ""
 			tokens = input.split(" ")
 			playerName = tokens[0]
 
 			tokens.delete_at(0)
 
 			numbers = tokens.map {|n| Integer(n)}
-			cards = cardsDisplayed.filter {|_, i| numbers.include?(i)}
+			cards = []
+			
+			for i in 0...$cardsDisplayed.length
+				if numbers.include?(i)
+					cards.push($cardsDisplayed[i])
+				end
+			end
 
 			if isSet(cards[0], cards[1], cards[2])
-				player = players.find {|p| p.name == playerName}
+				player = $players.find {|p| p.name == playerName}
 
-				puts "This is a set! #{player.name} has scored #{++player.score} times!"
+				player.score += 1
+				puts "This is a set! #{player.name} has scored #{player.score} times!"
 
-				if cardsDisplayed.length > 12
+				if $cardsDisplayed.length > 12
 					addThreeDisplayedCardsToUsed(cards[0], cards[1], cards[2])
 				else
 					addThreeDisplayedCardsToUsed(cards[0], cards[1], cards[2])
 					addRandomAvailableCardsToDisplayed(3)
-                end
+				end
 			else
 				puts "Not a set, try again"
-            end
+			end
 		else
-			if cardsAvailable.length >= 3
+			if $cardsAvailable.length >= 3
 				puts "Okay...here's some more"
 				addRandomAvailableCardsToDisplayed(3)
 			else
@@ -136,7 +146,7 @@ def playSet()
 end
 
 def generatePlayers()
-	players.push(Player.new("nick"), Player.new("michael"))
+	$players.push(Player.new("nick"), Player.new("michael"))
 end
 
 def startGames()
@@ -148,13 +158,12 @@ def startGames()
 
 		playSet()
 
-		winner = players.reduce {|mostPoints, currentPoints| currentPoints.score > mostPoints.score ? currentPoints : mostPoints }
+		winner = $players.reduce {|mostPoints, currentPoints| currentPoints.score > mostPoints.score ? currentPoints : mostPoints }
 
-		puts(
-			"Game over... #{winner.name} won with #{winner.score} points! #{winner.name} has won #{++winner.winCount} times."
-		)
+		winner.winCount += 1
+		puts "Game over... #{winner.name} won with #{winner.score} points! #{winner.name} has won #{winner.winCount} times."
 
-		players.each {|player| player.score = 0}
+		$players.each {|player| player.score = 0}
 
 		unless prompt("Continue playing?")
 			puts "Ending the game"
