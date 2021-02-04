463bb930fcf280b8c6cfb4872a1d6988e14e22d4
EstherH1 <hu.1808@buckeyemail.osu.edu>
2021-02-04 15:16:14 -0500

    updated test case card strings to match ones in set.rb

--- tests/test_set.rb
+++ tests/test_set.rb
@@ -4,9 +4,9 @@ class TestCard < MiniTest::Test
 
 #tests if addThreeDisplayedCardsToUsed will add the 3 given cards to the given array
 def test_addThreeDisplayedCardsToUsed
-    @cardOne = Card.new "pink", "oval", "shaded", 1 
-    @cardTwo = Card.new "blue", "wavy", "solid", 2
-    @cardThree= Card.new "green", "diamond", "hollow", 3
+    @cardOne = Card.new "purple", "oval", "striped", 1 
+    @cardTwo = Card.new "red", "squiggly", "solid", 2
+    @cardThree= Card.new "green", "diamond", "empty", 3
 
     originalArray = set.cardsUsed.push(cardOne, cardTwo, cardThree)
     returnedArray = set.addThreeDisplayedCardsToUsed(cardOne, cardTwo, cardThree)
@@ -17,17 +17,17 @@ end
 #tests if cardsAvailable will end up containing all elements from both cardsUsed and cardsDisplayed
 #will not check order since order is randomly shiffled #####################
 def test_shuffleDeck
-    @card = Card.new "pink", "oval", "hollow", 1
+    @card = Card.new "purple", "oval", "empty", 1
     assert_equal 1, @card.number
 end 
 
 #tests if 3 given cards are a set. Here they are.
 def test_addThreeDisplayedCardsToUsed
-    @cardOne = Card.new "pink", "oval", "shaded", 1
-    @cardTwo = Card.new "blue", "wavy", "solid", 2
-    @cardThree= Card.new "green", "diamond", "hollow", 3
+    @cardOne = Card.new "purple", "oval", "striped", 1
+    @cardTwo = Card.new "red", "squiggly", "solid", 2
+    @cardThree= Card.new "green", "diamond", "empty", 3
 
-    isSetActual = false
+    isSetActual = true
     isSetReturned = set.isSet(cardOne, cardTwo, cardThree)
 
     assert_equal isSetReturned, isSetActual
@@ -35,9 +35,9 @@ end
 
 #tests if 3 given cards are a set. Here they are not.
 def test_addThreeDisplayedCardsToUsed
-    @cardOne = Card.new "pink", "oval", "shaded", 1
-    @cardTwo = Card.new "blue", "diamond", "solid", 2
-    @cardThree= Card.new "green", "diamond", "hollow", 3
+    @cardOne = Card.new "purple", "oval", "striped", 1
+    @cardTwo = Card.new "red", "diamond", "solid", 2
+    @cardThree= Card.new "green", "diamond", "empty", 3
 
     isSetActual = false
     isSetReturned = set.isSet(cardOne, cardTwo, cardThree)
